56d4cec2c25eddd0a47d88fa05acbe0df9dea51b
Arseni Zhukov <43505164+lce-Cream@users.noreply.github.com>
2021-04-17 18:07:44 +0300

    Update dict.sh

--- dict.sh
+++ dict.sh
@@ -3,16 +3,16 @@ help(){
 echo "Usage: dict [word] [options...]
 Displays a couple of definitions for a given word in english divided by # sign.
 
- --help 	Get help message"
+ -h, --help 	Get help message"
 }
 
-if [[ $1 == "--help" ]]; then
+if [[ $1 == "--help" || $1 == "-h" ]]; then
 	help
 	exit
 fi
 
 URL_START="https://www.dictionaryapi.com/api/v3/references/learners/json/"
-URL_END="?key=2a971670-b122-49a2-b2df-487e11594e0e"
+URL_END="?key=API key here"
 WORD=$1
 URL="$URL_START$WORD$URL_END"
 
